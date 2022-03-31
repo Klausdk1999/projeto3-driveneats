ca60bbb3c823788b26c22919d0c405ceb911ee9d
KlausDk <klausdk1999@gmail.com>
2022-03-31 13:46:50 -0300

    bordas resetando on click

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
 let mainPlate;
+let mainDrink;
+let mainDessert;
+const plates=["pizza","frango","lanche","sushi","batata"];
+const drinks=["coca","guarana","agua","suco"];
+const desserts=["brigadeiro","pizza-doce","bolo"];
 
-function plate(x){
-    mainPlate=x;
-    document.getElementById(x).style.borderColor="green";
-    //alert("Voce escolheu "+mainPlate);
+//select recebe x=id do produto e y=tipo do produto
+function select(x,y){
+    if(y==plates){
+        mainPlate=x;
+    }
+    if(y==drinks){
+        mainDrink=x;
+    }
+    if(y==desserts){
+        mainDessert=x;
+    }
+
+    for(let i=0;i<y.length;i++){
+        if(x==y[i]){
+            document.getElementById(x).style.borderColor="green";
+        }else{
+            document.getElementById(y[i]).style.borderColor="white";
+        }
+    }
 }
