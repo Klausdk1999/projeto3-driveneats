c9d109d52a8fae5ed3fa0072e75f2ef4823966cb
KlausDk <klausdk1999@gmail.com>
2022-03-31 17:20:19 -0300

    bug no envio zap

--- scripts.js
+++ scripts.js
@@ -1,20 +1,23 @@
-let mainPlate;
-let mainDrink;
-let mainDessert;
+let mainPlate="";
+let mainDrink="";
+let mainDessert="";
 const plates=["pizza","frango","lanche","sushi","batata"];
+const platesV=['34.9','24.9','19.9','19.9','14.9'];
 const drinks=["coca","guarana","agua","suco"];
+const drinksV=['7.9','4.9','6.9','4.9'];
 const desserts=["brigadeiro","pizza-doce","bolo"];
+const dessertsV=['4.9','29.9','7.9'];
 
 //select recebe x=id do produto e y=tipo do produto
 function select(x,y){
     if(y==plates){
-        mainPlate=x;
+        mainPlate=String(x);
     }
     if(y==drinks){
-        mainDrink=x;
+        mainDrink=String(x);
     }
     if(y==desserts){
-        mainDessert=x;
+        mainDessert=String(x);
     }
     let z="i"+x;
     for(let j=0;j<y.length;j++){
@@ -28,10 +31,20 @@ function select(x,y){
     }
     order();
 }
+//order verifica se os 3 produtos foram escolhidos para modificar o botão order
 function order(){
     if(mainPlate!=null && mainDrink!=null && mainDessert!=null ){
         document.getElementById("orderTxt").style.display="none";
         document.getElementById("closeOrder").style.display="block";
         document.getElementById("order").style.backgroundColor="green";
     }
+}
+
+let t1=encodeURIComponent("Olá, gostaria de fazer o pedido:\n- Prato: ");
+let t2=encodeURIComponent("\n- Bebida: ");
+let t3=encodeURIComponent("\n- Sobremesa: ");
+let t4=encodeURIComponent("\nTotal: ");
+
+function whatsapp(){
+    window.open("https://wa.me/+5547997709595?text="+t1+mainPlate+t2+mainDrink+t3+mainDessert+t4+total);
 }
